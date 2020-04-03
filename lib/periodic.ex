389aa4db551b60222910085bc0372ef979e0a550
Lok Chun Wai <cwlok0613@gmail.com>
2020-04-03 16:45:53 +0800

    First commit

---
+++ lib/periodic.ex
@@ -0,0 +1,15 @@
+defmodule Periodic do
+
+  def get_daily(objects) do
+    objects
+  end
+
+  def get_weekly(objects) do
+
+  end
+
+  def get_monthly(objects) do
+
+  end
+
+end
---
+++ test/periodic_test.exs
@@ -0,0 +1,17 @@
+defmodule PeriodicTest do
+  use ExUnit.Case
+  # doctest Periodic
+
+  test "get_daily/1" do
+
+  end
+
+  test "get_weekly/1" do
+
+  end
+
+  test "get_monthly/1" do
+
+  end
+
+end
